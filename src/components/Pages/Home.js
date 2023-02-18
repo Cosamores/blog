63024d5ef59816d6a18b57c90f2e20072b22e8b0
Diego Soares <diegocosamores@gmail.com>
2023-02-18 03:04:26 -0300

    Added the SoundCloud API

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -21,7 +21,10 @@ const Home = (props) => {
     <div className={styles.home}>
       <Navbar />
       <ImageCarousel images={images} src={images.src} alt={images.alt}/>
-          </div>
+      <footer>        
+        <iframe width="100%" height="166" scrolling="no" title="soundcloud" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/769311256&color=%234e3086&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style={{fontSize: "10px", color: "#cccccc", lineBreak: "anywhere", wordBreak: "normal", overflow: "hidden", whiteSpace: "nowrap", TextOverflow: "ellipsis", fontFamily: "Interstate, Lucida Grande, Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif", fontWeight: "100"}}><a href="https://soundcloud.com/diegocosamores" title="COSAMORES" target="_blank" rel="noreferrer" style={{color: "#cccccc", textDecoration: "none"}}>COSAMORES</a> · <a href="https://soundcloud.com/diegocosamores/possa-ser" title="Possa Ser" target="_blank" rel="noreferrer" style={{color: "#cccccc", textDecoration: "none"}}>Possa Ser</a></div>
+      </footer>
+    </div>
 
     )
 
