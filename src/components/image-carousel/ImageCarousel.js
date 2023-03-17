3329d696fb9bf9b0135dea3db20fb7bb083000b5
Diego Soares <diegocosamores@gmail.com>
2023-03-17 16:10:21 -0300

    loading local image on image carroussel

--- src/components/image-carousel/ImageCarousel.js
+++ src/components/image-carousel/ImageCarousel.js
@@ -24,7 +24,7 @@ const ImageCarousel = ({ images }) => {
         {images.map((image, index) => (
           <img
             key={image.alt}
-            src={image.src}
+            src={require(image.src)}
             alt={image.alt}
             className={`ImageCarousel-image ${
               index === currentIndex ? "active" : ""
